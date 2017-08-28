73d1fbf3fb59a1ad67a31a852c1871a2f22f7bef
Peter Elbaum <elbaumpj@gmail.com>
2017-08-28 16:20:38 -0400

    playing with routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('waka flocka flayy');
+  //res.send('waka flocka flayy');
+
+  const peter = {name: 'Peter', age: 25, cool: true}
+  //res.json(peter); 
+  res.json(req.query); 
+});
+
+router.get('/reverse/:name', (req, res) => {
+
+  //console.log([...req.params.name]); //spread operator, essentially the same as split(''); 
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 

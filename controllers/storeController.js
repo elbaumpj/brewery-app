d2ce257e894c0988d613e5f381f10ff843e66a30
Peter Elbaum <elbaumpj@gmail.com>
2017-09-27 16:45:03 -0400

    edit flow for store models done

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -22,3 +22,24 @@ exports.getStores = async (req, res) => {
     const stores = await Store.find(); 
     res.render('stores', {title: 'Stores', stores}); 
 }
+
+exports.editStore = async (req, res) => {
+    //1. find store given id
+    const store = await Store.findOne({ _id: req.params.id }); 
+    //2. confirm they are owner of store
+
+    //3. render out edit form so user can update their store
+    res.render('editStore', {title: `Edit ${store.name}`, store }); 
+}
+
+exports.updateStore = async (req, res) => {
+    //1. find and update store
+    const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+        new: true, 
+        runValidators: true
+    }).exec();
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>.`)
+    res.redirect(`/stores/${store._id}/edit`); 
+
+    //2. redirect user to store and tell them it worked 
+}

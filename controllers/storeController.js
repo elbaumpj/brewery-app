8e0895c1fea7f4d3b88a3485654fb0171b594f0a
elbaumpj <elbaumpj@gmail.com>
2017-10-04 18:13:06 -0400

    setting location data to be a point for maps visualization again

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -34,7 +34,7 @@ exports.editStore = async (req, res) => {
 
 exports.updateStore = async (req, res) => {
     //set the location data to be a point
-    req.body.location.type = 'Point'; 
+    req.body.location.type = 'Point';   
     //1. find and update store
     const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
         new: true, 

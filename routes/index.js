7137cf6b2b00e197d67545f98e8dd45a50b89fc4
elbaumpj <elbaumpj@gmail.com>
2017-10-20 14:58:25 -0400

    working on user model

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController'); 
+const userController = require('../controllers/userController'); 
 const { catchErrors } = require('../handlers/errorHandlers'); 
 
 // Do work here
@@ -25,7 +26,12 @@ catchErrors(storeController.updateStore)
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug)); 
 
 router.get('/tags', catchErrors(storeController.getStoresByTag)); 
+
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag)); 
+
+// user
+
+router.get('/login', userController.loginForm); 
 
 
 module.exports = router;

a6f6419a2ccfb3a5ebba1ff6c1a0f59900fed3e8
elbaumpj <elbaumpj@gmail.com>
2017-10-26 16:43:52 -0400

    forgot password mixin set, working on email flow

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const passport = require('passport'); 
+const mongoose = require('mongoose'); 
+const User = mongoose.model('User'); 
 
 exports.login = passport.authenticate('local', {
     failureRedirect: '/login',
@@ -20,4 +22,16 @@ exports.isLoggedIn = (req, res, next) => {
     }
     req.flash('error', 'Oops! You must be logged in to do that!'); 
     res.redirect('/login'); 
+}; 
+
+exports.forgot = async (req, res) => {
+    // see if user w/ that email exists
+    const user = await User.findOne({ email: req.body.email }); 
+    if (!user) {
+        req.flash('error', 'No account with that email exists'); 
+        return res.redirect('/login'); 
+    }
+    // set reset tokens and expiry on their account
+    // send an email with token
+    // redirect to login page
 }; 
